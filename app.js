9b2a7a1aaba9040293b25ed75cf274ae86f56b1f
Raul <00004017@uca.edu.sv>
2018-11-17 11:04:19 -0600

    configuraciones

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -8,9 +9,28 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+const MongoStore = require('connect-mongo')(session);
+//Credenciales de nuestra base de datos
+const { mongodb } = require('./configs/keys');
+const session = require('express-session');
+
+
 var app = express();
 
+require('./configs/database');
+
 // view engine setup
+app.use(session({
+    secret:"Hello World!!!",
+    resave: true, // para alamcenar el objeto session
+    saveUninitialized: true, // inicializar si el objeto esta vacio
+    //para almacenar la sesion en la base de datos
+    store: new MongoStore({
+        url: mongodb.URI,
+        autoReconnect: true
+    })
+    }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -26,7 +46,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -37,7 +57,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,7 +68,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
