1669d7b9ea0fec9d0044695428604926b2493c89
Raul <00004017@uca.edu.sv>
2018-11-22 15:47:03 -0600

    Se añaden nuevos controladores para el post

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,42 +2,69 @@
 //probar en postman
 
 const mongoose = require('mongoose'),
-    postModel=require('../models/post')
+    postModel = require('../models/post')
 
-const PostController={
+const PostController = {
 };
 
-PostController.create=function(req,res){
+PostController.create = function (req, res) {
     res.json({
         okay=true
     });
     //codigo de obtener datos de la peticion 
-    let data={
-        nombre:req.body.nombre,
-        autor:req.body.autor
+    let data = {
+        nombre: req.body.nombre,
+        autor: req.body.autor
     }
     //validar valores 
-    if(data.nombre && data.autor && data.nombre !='' && data.autor ){
+    if (data.nombre && data.autor && data.nombre != '' && data.autor) {
 
-    }else{
+    } else {
         res.status(400);
-        res.jason({err:{code:400,message:'faltan datos'}});
+        res.jason({ err: { code: 400, message: 'faltan datos' } });
     }
     //crear un objeto post
-    let nuevoPost= new postModel(data);
+    let nuevoPost = new postModel(data);
     nuevoPost.save(function (err) {
-        if(err){
+        if (err) {
             res.status(500);
-        }else{
-            res.json({ok:true,message:'se guardo con exito'})
+        } else {
+            res.json({ ok: true, message: 'se guardo con exito' })
         }
     })
     //guardar en la base de datos 
 };
 
+PostController.update = function (params) {
+    let update={
+        nombre: req.body.nombre,
+        autor: req.body.autor
+    }
+    //ejecuta la update 
+    postModel.findByIdAndUpdate(req.params.id, update, function (err, old) {
+        if (err) {
+            res.status(500);
+        } else {
+            res.json({ ok: true,old,update})
+        }
+    })
+
+}
+
+PostController.delete=function (params) {
+    postModel.findByIdAndRemove(req.params.id, update, function (err, old) {
+        if (err) {
+            res.status(500);
+        } else {
+            res.json({ ok: true,old,update})
+        }
+    })
+}
 
 
-PostController.read=function(req,res){
+
+
+PostController.read = function (req, res) {
     //obtener todos los post de la base de datos y enviarlos como respuesta json
 };
-module.exports=PostController;
+module.exports = PostController;
