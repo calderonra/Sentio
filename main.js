93975d6649a9ef81dae5898b65a5fb9620ef4da2
RoxanaCortez <00502211@uca.edu.sv>
2018-11-21 18:59:50 -0600

    capturando evento

---
+++ main.js
@@ -0,0 +1,25 @@
+window.onload = ()=> {
+    app.init();
+}
+let app = {
+    init:function(){
+        //alert("Estoy presionando el boton");
+        this.addEvents();
+        this.loadContent();
+    },
+    addEvents:function(){
+        document.postForm.addEventListener('submit',this.submitPost);
+    },
+    submitPost:function(event){  //Capturando el evento
+        event.preventDefault();
+        alert('Estoy presionando el boton');
+    },
+    loadContent:function(){
+        fetch('/post',{
+            method:'GET'
+        }).then(res => {
+            return res.json()
+        })
+    }
+};
+
