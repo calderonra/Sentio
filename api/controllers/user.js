9535365ab804e01d2c57b1e5da788271a1240a77
00115417@uca.edu.sv <00004017@uca.edu.sv>
2018-11-29 19:36:53 -0600

    Métodos para subir archivos y obtener colección de usuarios

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,4 +1,7 @@
 "use strict"
+var mongoosePaginate = require('mongoose-pagination');
+var fs = require('fs');
+var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
@@ -14,7 +17,7 @@ function pruebas(req, res) {
         message: 'hola mundo desde el server'
     });
 }
-
+//Registrar
 function saveUser(req, res) {
     var params = req.body;
     var user = new User();
@@ -61,39 +64,145 @@ function saveUser(req, res) {
         });
     }
 }
-
-function loginUser(req,res){
+//Login
+function loginUser(req, res) {
     var params = req.body;
 
     var email = params.email;
     var password = params.password;
 
-    User.findOne({email:email},(err,user)=>{
-         
-        
-        if (err) return res.status(500).send({message: 'Error en la peticion'});
-    
-        if(user){
-            bcrypt.compare(password, user.password,(err, check)=>{
-                if (check){
-                    if(params.gettoken){
+    User.findOne({ email: email }, (err, user) => {
+
+
+        if (err) return res.status(500).send({ message: 'Error en la peticion' });
+
+        if (user) {
+            bcrypt.compare(password, user.password, (err, check) => {
+                if (check) {
+                    if (params.gettoken) {
                         //devolver token
                         //generar token
                         return res.status(200).send({
                             token: jwt.createToken(user)
-                        }); 
-                    }else{
+                        });
+                    } else {
                         //devuelve los datos del usuario
-                    user.password = undefined;
-                    return res.status(200).send({user});
+                        user.password = undefined;
+                        return res.status(200).send({ user });
                     }
                 }
-                else{
-                    return res.status(404).send({message: 'El usario no se ha podido indentifcar'})
+                else {
+                    return res.status(404).send({ message: 'El usario no se ha podido indentifcar' })
                 }
             });
+        } else {
+            return res.status(404).send({ message: 'El usario no se ha podido indentifcar' })
+        }
+    });
+}
+
+//Conseguir datos de un usuario
+function getUser(req, res) {
+    var userId = req.params.id;
+
+    User.findById(userId, (err, user) => {
+        if (err) return res.status(500).send({ message: 'Error en la petición' });
+
+        if (!user) return res.status(404).send({ message: 'El usuario no existe' });
+
+        return res.status(200).send({ user });
+    });
+}
+
+
+//Devolver un listado de usuarios paginados
+function getUser(req, res) {
+    var identity_user_id = req.user.sub;
+
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 5;
+
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+        if (err) return res.status(500).send({ message: 'Error en la petición' });
+        if (!users) return res.status(404).send({ message: 'No hay usuarios disponibles en la plataforma' });
+
+        return res.status(200).send({
+            users,
+            total,
+            pages: Math.ceil(total / itemsPerPage)
+        });
+    });
+}
+
+//Edición de datos de usuarios
+function updateUser(req, res) {
+    var userId = req.params.id;
+    var update = req.body;
+
+    //Borrar la propiedad: 'password'
+    delete update.password;
+
+    //Comprobar si es el propio usuario quien va a hacer la modifiación
+    if (userId != req.user.sub) {
+        return res.status(500).send({ message: 'No tienes permiso para actualizar los datos del usuario' });
+    }
+
+    User.findByIdAndUpdate(userId, update, { new: true }, (err, userUpdated) => {
+        if (err) return res.status(500).send({ message: 'Error en la petición' });
+        if (!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar el usuario' });
+
+        return res.status(200).send({ user: userUpdated });
+    });
+}
+
+//Subir archivos de imagen/avatar de usuario
+function uploadImage(req, res) {
+    var userId = req.params.id;
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.file_split('/');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (userId != req.user.sub) {
+            return removeFilesOfUploads(res, file_path, 'No tienes permiso para actualizar los datos del usuario');
+        }
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+            //Actualizar documento de usuario logueado
+            User.findByIdAndUpdate(userId, update, { new: true }, (err, userUpdated) => {
+                if (err) return res.status(500).send({ message: 'Error en la petición' });
+                if (!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar el usuario' });
+
+                return res.status(200).send({ user: userUpdated });
+            });
         }else{
-            return res.status(404).send({message: 'El usario no se ha podido indentifcar'})
+            return removeFilesOfUploads(res,file_path,'Extensión no válida');
+        }
+    }else{
+        return res.status(200).send({message: 'No se han subido imágenes'});
+    }
+}
+
+function removeFilesOfUploads(res,file_path,message){
+    fs.unlink(file_path,(err)=>{
+        return res.status(200).send({message:message})
+    });
+}
+
+function getImageFile(req,res){
+    var image_file = req.params.imageFile;
+    var path_file = './uploads/users/'+image_file;
+
+    fs.exists(path_file,(exists)=>{
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message:'No existe la imagen'});
         }
     });
 }
@@ -102,5 +211,10 @@ module.exports = {
     home,
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    getUser,
+    getUsers,
+    updateUser,
+    uploadImage,
+    getImageFile
 }
